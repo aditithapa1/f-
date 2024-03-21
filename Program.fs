33401ef2ba543086953a4cbd54e32f3d177e4722
Aditi <Aditi.Thapa@triosstudent.com>
2024-03-21 15:56:26 -0400

    first commit

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from Aditi Thapa"
+let rec factorial number secondparameter =
+    if number = 0 then
+        secondparameter
+    else
+        factorial (number - 1) (secondparameter * number)
+
+let result = factorial 4 1
+printfn "Factorial of 4 is: %d" result
